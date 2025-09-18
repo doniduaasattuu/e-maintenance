ab8dd0b9c198d9031883a942dcf502ed8a46eb37
Doni Darmawan <doni.duaasattuu@gmail.com>
2025-09-18 07:11:08 +0700

    fix: equipment_image table name migration

--- database/migrations/2025_08_19_052735_create_equipment_images_table.php
+++ database/migrations/2025_08_19_052735_create_equipment_images_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('equipment_images');
+        Schema::dropIfExists('equipment_image');
     }
 };
